fcda2c8ef280645cf5daa286afb678fe4c8cabd7
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2023-04-04 16:00:51 -0400

    redid db files

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,32 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE employee_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT Null,
     role_id INT,
-    INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    manager_id INTEGER,
-    INDEX manager_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
 )
--- db/seed.sql
+++ db/seed.sql
@@ -1,28 +1,31 @@
-INSERT INTO department (name)
+INSERT INTO
+    department (name)
 VALUES
-('IT'),
-('Finance & Accounting'),
-('Sales & Marketing'),
-('Operations');
+    ('IT'),
+    ('Finance & Accounting'),
+    ('Sales & Marketing'),
+    ('Operations');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO
+    roles (title, salary, department_id)
 VALUES
-('Full Stack Developer', 80000, 1),
-('Software Engineer', 120000, 1),
-('Accountant', 10000, 2),
-('Financial Analyst', 150000, 2),
-('Marketing Coordinator', 70000, 3),
-('Sales Lead', 90000, 3),
-('Project Manager', 100000, 4),
-('Operators Manager', 90000, 4);
+    ('Full Stack Developer', 80000, 1),
+    ('Software Engineer', 120000, 1),
+    ('Accountant', 10000, 2),
+    ('Financial Analyst', 150000, 2),
+    ('Marketing Coordinator', 70000, 3),
+    ('Sales Lead', 90000, 3),
+    ('Project Manager', 100000, 4),
+    ('Operators Manager', 90000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO
+    employees (first_name, last_name, role_id, manager_id)
 VALUES
-('Joon', 'Kim', 2, null),
-('Abdiel', 'Villalobos', 1, 1),
-('Eli', 'Hemesath', 4, null),
-('Eric', 'Fritz', 3, 3),
-('Matt', 'Hemsley', 6, null),
-('Sarah', 'Riser', 5, 5),
-('Taylor', 'Pedmo', 7, null),
-('Kshiya', 'Hines', 8, 7);
+    ('Joon', 'Kim', 1, null),
+    ('Abdiel', 'Villalobos', 2, 1),
+    ('Eli', 'Hemesath', 3, null),
+    ('Eric', 'Fritz', 4, 3),
+    ('Matt', 'Hemsley', 5, null),
+    ('Sarah', 'Riser', 6, 5),
+    ('Taylor', 'Pedmo', 7, null),
+    ('Kshiya', 'Hines', 8, 7);
